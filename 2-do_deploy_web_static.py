a14cad40d359cc5f94877a5636e0d18836ec3008
rodynaamrfathy <rodynaamr@icloud.com>
2024-05-13 12:01:33 -0700

    ‘UPDATE’

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import env
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ["52.91.116.70", "18.214.87.35"]
 
 
 def do_deploy(archive_path):
